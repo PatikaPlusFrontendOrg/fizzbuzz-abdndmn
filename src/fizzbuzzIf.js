a467c8d31693cc6e623edefbaa8b6aa1e5357dda
abdndmn <abdndmn@hotmail.com>
2025-05-23 01:35:34 +0300

    if and switch codes were added

--- src/fizzbuzzIf.js
+++ src/fizzbuzzIf.js
@@ -1,5 +1,15 @@
 function fizzbuzzIf(num) {
     // Kodlar buraya gelecek
+    if( num % 3 === 0 && num % 5 === 0) {
+        return "FizzBuzz";
+    }else if(num % 5 === 0){
+        return "Buzz";
+    }
+    else if(num% 3===0){
+        return "Fizz";
+    }else{
+        return num;
+    }
 }
 
 module.exports = fizzbuzzIf;
--- src/fizzbuzzSwitch.js
+++ src/fizzbuzzSwitch.js
@@ -1,5 +1,15 @@
 function fizzbuzzSwitch(num) {
     // Kodlar buraya gelecek
+    switch (true) {
+        case num % 3 === 0 && num % 5 === 0:
+            return "FizzBuzz";
+        case num % 3 === 0:
+            return "Fizz";
+        case num % 5 === 0:
+            return "Buzz";
+        default:
+            return num;
+    }
 }
 
 module.exports = fizzbuzzSwitch;
